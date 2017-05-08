bb519216e04dc91fd83f20b0fdd83850ea430c7a
j.drake.hoffman@gmail.com <j.drake.hoffman@gmail.com>
2017-05-08 19:36:35 -0400

    remove sleep statement

--- scratchpad.sh
+++ scratchpad.sh
@@ -3,6 +3,6 @@ i3-msg floating enable
 i3-msg border normal 5
 xdotool getactivewindow windowsize 1200 700
 xdotool getactivewindow windowmove 5400 1740
-sleep 1
+#sleep 1
 i3-msg sticky enable
 i3-msg move scratchpad
