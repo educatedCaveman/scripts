a058a061354ecaecb818d7ab586459a433df4d15
j.drake.hoffman@gmail.com <j.drake.hoffman@gmail.com>
2018-09-02 13:41:31 -0400

    updates for ubuntu18.04 using gnome

--- gnomeAndNitrogen.sh
+++ gnomeAndNitrogen.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #unity-settings-daemon &
-gnome-settings-daemon &
-sleep 3	#might break the script
+#gnome-settings-daemon &
+gnome-session 
+#sleep 3	#might break the script
 
 #xrandr
-#xrandr --output DFP9 --rotate left pos 0x0 --output DFP59 pos 1440x200 --primary --output DFP1 --rotate left --pos 5280x0
+#xrandr --output DisplayPort-5 --rotate left --pos 0x0 --output DisplayPort-3 --pos 1440x0 --primary --output DisplayPort-4 --rotate right --pos 5280x0
+
+sleep 1
 
 nitrogen --restore
