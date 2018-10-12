4ba9521e7f59e984cdb9b9ffebf6213c5c9fc19b
j.drake.hoffman@gmail.com <j.drake.hoffman@gmail.com>
2018-10-12 12:27:26 -0400

    reset monitors

--- gnomeAndNitrogen.sh
+++ gnomeAndNitrogen.sh
@@ -5,7 +5,7 @@ gnome-session
 #sleep 3	#might break the script
 
 #xrandr
-#xrandr --output DisplayPort-5 --rotate left --pos 0x0 --output DisplayPort-3 --pos 1440x0 --primary --output DisplayPort-4 --rotate right --pos 5280x0
+xrandr --output DisplayPort-5 --rotate left --pos 0x0 --output DisplayPort-3 --pos 1440x0 --primary --output DisplayPort-4 --rotate right --pos 5280x0
 
 sleep 1
 
