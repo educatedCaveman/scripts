61b65af1159f572d646ebb59a2884ce6f6b4b18c
Drake <j.drake.hoffman@gmail.com>
2019-03-09 13:33:49 -0500

    only issue the reminder when nvim is installed

--- neovim_reminder.sh
+++ neovim_reminder.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 #reminder to use neovim over vim
 
+#check if neovim is even an option:
+which nvim > /dev/null 2>&1
+NVERR=$?
+if [ $NVERR -ne "0" ];
+then
+    #echo "no neovim"
+    vim "$@"
+    exit 0
+fi
+
 echo "you should use Neovim instead of Vim."
 echo "run 'nvim <file>' instead."
 echo "opening Vim in..."
@@ -11,4 +21,4 @@ printf "2..."
 sleep 1
 printf "1...\n"
 sleep 1
-vim $1
+vim "$@"
