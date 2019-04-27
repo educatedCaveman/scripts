6597e1c9c6f25d7cdd289e0f0a1c7ebd1acce5de
Drake <j.drake.hoffman@gmail.com>
2019-04-27 11:00:43 -0400

    add error handling

--- mobius/bridges.sh
+++ mobius/bridges.sh
@@ -7,13 +7,22 @@ BRIDGE0="bridge0"
 BRIDGE1="bridge1"
 BRIDGE2="bridge2"
 
+timeout=10
+
 #wait for lagg0 to be up:
+cntr=0
 while :; do
     if [ "$(/sbin/ifconfig | /usr/bin/grep ^${LAGG0}:)" ];
     then
         break
     fi
-    sleep 1
+    ((cntr++))
+    if [ $cntr -ge $timeout ];
+    then
+        exit 1
+    else
+        sleep 1
+    fi
 done
 
 #wait for lagg1 to be up:
@@ -22,7 +31,13 @@ while :; do
     then
         break
     fi
-    sleep 1
+    ((cntr++))
+    if [ $cntr -ge $timeout ];
+    then
+        exit 1
+    else
+        sleep 1
+    fi
 done
 
 #wait for lagg2 to be up
@@ -31,22 +46,72 @@ while :; do
     then
         break
     fi
-    sleep 1
+    ((cntr++))
+    if [ $cntr -ge $timeout ];
+    then
+        exit 1
+    else
+        sleep 1
+    fi
 done
 
 #until /sbin/ifconfig | /usr/bin/grep -q "^${LAGG}:"; do sleep 1; done
 sleep 1
 #destroy the bridges to undo what is done by default:
-/sbin/ifconfig "${BRIDGE0}" destroy
-/sbin/ifconfig "${BRIDGE1}" destroy
-/sbin/ifconfig "${BRIDGE2}" destroy
+if [ "$(/sbin/ifconfig | /usr/bin/grep ^${BRIDGE0}:)" ];
+    /sbin/ifconfig "${BRIDGE0}" destroy
+fi
+if [ "$(/sbin/ifconfig | /usr/bin/grep ^${BRIDGE1}:)" ];
+    /sbin/ifconfig "${BRIDGE1}" destroy
+fi
+if [ "$(/sbin/ifconfig | /usr/bin/grep ^${BRIDGE2}:)" ];
+    /sbin/ifconfig "${BRIDGE2}" destroy
+fi
+#exit if the bridges still exist:
+if [ "$(/sbin/ifconfig | /usr/bin/grep ^${BRIDGE0}:)" ];
+    exit 1
+fi
+if [ "$(/sbin/ifconfig | /usr/bin/grep ^${BRIDGE1}:)" ];
+    exit 1
+fi
+if [ "$(/sbin/ifconfig | /usr/bin/grep ^${BRIDGE2}:)" ];
+    exit 1
+fi
+
+#/sbin/ifconfig "${BRIDGE0}" destroy
+#/sbin/ifconfig "${BRIDGE1}" destroy
+#/sbin/ifconfig "${BRIDGE2}" destroy
 #recreate them:
 /sbin/ifconfig "${BRIDGE0}" create
+if [ $? -ne 0 ];
+then
+    exit 1
+fi
 /sbin/ifconfig "${BRIDGE1}" create
+if [ $? -ne 0 ];
+then
+    exit 1
+fi
 /sbin/ifconfig "${BRIDGE2}" create
+if [ $? -ne 0 ];
+then
+    exit 1
+fi
 #and add the laggs to them
 /sbin/ifconfig "${BRIDGE0}" addm "${LAGG0}" up
+if [ $? -ne 0 ];
+then
+    exit 1
+fi
 /sbin/ifconfig "${BRIDGE1}" addm "${LAGG1}" up
+if [ $? -ne 0 ];
+then
+    exit 1
+fi
 /sbin/ifconfig "${BRIDGE2}" addm "${LAGG2}" up
+if [ $? -ne 0 ];
+then
+    exit 1
+fi
 exit 0
 
