c1e24dfb3fbca778689b3fe29149e3339bcbeae4
drake <j.drake.hoffman@gmail.com>
2020-10-29 20:51:10 -0400

    formatting

--- mobius/set_iDRAC_fans_20.sh
+++ mobius/set_iDRAC_fans_20.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #set the fn speed of mobius to 20%
-ipmitool -I lanplus -H mobius.mgmt -U idrac_remote -P password raw 0x30 0x30 0x01 0x00              
+ipmitool -I lanplus -H mobius.mgmt -U idrac_remote -P password raw 0x30 0x30 0x01 0x00
 ipmitool -I lanplus -H mobius.mgmt -U idrac_remote -P password raw 0x30 0x30 0x02 0xff 0x14
