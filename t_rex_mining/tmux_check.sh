fbdd4e9502bbf8d8400da62b1e7bcd1f5281b43e
Drake <j.drake.hoffman@gmail.com>
2021-03-21 13:33:04 -0400

    workaround for ruby being fucked

--- t_rex_mining/tmux_check.sh
+++ t_rex_mining/tmux_check.sh
@@ -4,6 +4,11 @@ tmux has-session 2>/dev/null
 
 if [ $? != 0 ]; then
     # start session if it doesn't exist
-    tmuxinator start moria
+    
+    # this is broken bc of ruby
+    # tmuxinator start moria
+
+    # this is a workaround for tmuxinator being broken
+    tmux new -s mining -d 'sh /home/drake/scripts/t_rex_mining/t_rex.sh; zsh'
 fi
 
