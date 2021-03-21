87708fc23cbe4a940253c21d046c506b2da3fe27
drake <j.drake.hoffman@gmail.com>
2021-03-20 20:54:11 -0400

    saitize

--- trex.sh
+++ trex.sh
@@ -4,8 +4,7 @@ TREX_PATH="/home/drake/t_rex"
 TREX_URL='ssl://us1.ethermine.org:5555'
 NAME="Moria"
 
-source "${SECRETS}"
+. "${SECRETS}"
 WALLET_ID="${ETHEREUM_WALLET_ID}"
 
-# ./t-rex -a ethash -o ssl://us1.ethermine.org:5555 -u 0xEeFCdd7FbCEE200627FB20a8A68B32bADA517003 -p x -w Moria
-sh "${TREX_PATH}/t-rex" -a ethash -o "${TREX_URL}" -u "${WALLET_ID}" -p x -w "${NAME}"
+"${TREX_PATH}/t-rex" -a ethash -o "${TREX_URL}" -u "${WALLET_ID}" -p x -w "${NAME}"
