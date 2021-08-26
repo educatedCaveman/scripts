74c3de96987bcd2214cd6853f48d6c0135f3da04
drake <j.drake.hoffman@gmail.com>
2021-08-25 20:13:14 -0400

    fix paths

--- cron/backup_docker_weekly.sh
+++ cron/backup_docker_weekly.sh
@@ -2,20 +2,20 @@
 # archive the /docker backup weekly
 
 HOST=$(/usr/bin/hostname)
-SRC="/mnt/mobius/Backup/docker/${HOST}/"
+SRC="/mnt/mobius/Backup/docker/${HOST}"
 DEST="/mnt/mobius/Backup/docker/archive/${HOST}/"
 
 # the following assumes that if $DEST exists, $SRC must also exist
-if [ -e "${DEST}" ]
+if [ -e "${DEST}/" ]
 then
     # remove the oldest archive
-    OLD_ARCH=$(ls -1q "${DEST}" | head -1)
-    rm "${OLD_ARCH}"
+    OLD_ARCH=$(ls -1q "${DEST}/" | head -1)
+    rm "${DEST}/${OLD_ARCH}"
 
     # create new archive
     DATE=$(date +%F-%T)
-    FULL_DEST="${DEST}${HOST}_docker.${DATE}.pigz.tar.gz"
-    /usr/bin/tar -cf "${FULL_DEST}" --use-compress-prog=pigz "${SRC}"
+    FULL_DEST="${DEST}/${HOST}_docker.${DATE}.pigz.tar.gz"
+    /usr/bin/tar -cf "${FULL_DEST}" --use-compress-prog=pigz "${SRC}/"
 fi
 
 
