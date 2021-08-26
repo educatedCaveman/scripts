d7aa3ae5078c3bf975f6d7fd5e2330598526d367
drake <j.drake.hoffman@gmail.com>
2021-08-25 20:09:26 -0400

    docker archive script

---
+++ cron/backup_docker_weekly.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# archive the /docker backup weekly
+
+HOST=$(/usr/bin/hostname)
+SRC="/mnt/mobius/Backup/docker/${HOST}/"
+DEST="/mnt/mobius/Backup/docker/archive/${HOST}/"
+
+# the following assumes that if $DEST exists, $SRC must also exist
+if [ -e "${DEST}" ]
+then
+    # remove the oldest archive
+    OLD_ARCH=$(ls -1q "${DEST}" | head -1)
+    rm "${OLD_ARCH}"
+
+    # create new archive
+    DATE=$(date +%F-%T)
+    FULL_DEST="${DEST}${HOST}_docker.${DATE}.pigz.tar.gz"
+    /usr/bin/tar -cf "${FULL_DEST}" --use-compress-prog=pigz "${SRC}"
+fi
+
+
