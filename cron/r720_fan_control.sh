ed21528c27fb4efc288fe8f84ca45ada23b9da4e
educatedCaveman <j.drake.hoffman@gmail.com>
2022-01-05 10:17:32 -0500

    add todo

--- cron/r720_fan_control.sh
+++ cron/r720_fan_control.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-source $HOME/.zsh_secrets
 IPMI_HOST="192.168.1.7"
 USER_NAME="idrac_remote"
-# PASS=${IDRAC_PW}    # get from .zsh_secrets
+# IDRAC_PW comes from .zsh_secrets
+source $HOME/.zsh_secrets
 
-echo $IPMI_HOST
-echo $USER_NAME
-echo $IDRAC_PW
-
-# ipmitool -I lanplus -H $IPMI_HOST -U $USER_NAME -P $IDRAC_PW raw 0x30 0x30 0x01 0x00
-# ipmitool -I lanplus -H $IPMI_HOST -U $USER_NAME -P $IDRAC_PW raw 0x30 0x30 0x02 0xff 0x14
+#TODO: add check for current state, and only update if not in desired state
+ipmitool -I lanplus -H $IPMI_HOST -U $USER_NAME -P $IDRAC_PW raw 0x30 0x30 0x01 0x00
+ipmitool -I lanplus -H $IPMI_HOST -U $USER_NAME -P $IDRAC_PW raw 0x30 0x30 0x02 0xff 0x14
