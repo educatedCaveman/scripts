bb7e26ea3585d1758d94dd5adbeff9bfa9b24c52
drake <j.drake.hoffman@gmail.com>
2022-12-29 11:52:14 -0500

    expand pihole backup script

--- slack/pihole_backup.py
+++ slack/pihole_backup.py
@@ -7,11 +7,13 @@ from glob import glob
 import subprocess
 # from slack_webhook import Slack
 import requests
+from tabulate import tabulate
 
 path = '/mnt/mobius/Backup/pihole'
 d = datetime.today()
 d_str = d.strftime("%F")
 pi_file = 'pi-hole-singularity-teleporter_{}_*.tar.gz'.format(d_str)
+version_file = 'new_version'
 #TODO: get pihole specific webhook
 # webhook = os.getenv('SLACK_WEBHOOK_PIHOLE')
 webhook = os.getenv('DISCORD_WEBHOOK_PIHOLE')
@@ -73,6 +75,85 @@ def backup_found():
     requests.post(webhook, data=content)
 
 
+def format_version(version_str):
+    version_list = []
+    for version in version_str.split('\n'):
+        if len(version) > 0:
+            version_list.append(version.split())
+    
+    version_dict = {
+        "pihole":   f"{version_list[0][4]}",
+        "adminLTE": f"{version_list[1][4]}",
+        "FTL":      f"{version_list[2][4]}",
+    }
+    return version_dict
+
+
+def version_check():
+    """
+    drake@singularity ~ > pihole -v -l
+        Latest Pi-hole version is v5.14.2
+        Latest AdminLTE version is v5.18
+        Latest FTL version is v5.20
+    drake@singularity ~ > pihole -v -c
+        Current Pi-hole version is v5.14.1
+        Current AdminLTE version is v5.17
+        Current FTL version is v5.19.2
+    """
+
+    # fetch the versions:
+    cur_ver = format_version(subprocess.check_output(['pihole', '-v', '-c'], text=True))
+    max_ver = format_version(subprocess.check_output(['pihole', '-v', '-l'], text=True))
+
+    # check versions
+    pihole_diff = False
+    admin_diff = False
+    ftl_diff = False
+
+    diff_details = []
+
+    if cur_ver['pihole'] != max_ver['pihole']:
+        pihole_diff = True
+        diff_details.append(['Pi-hole', cur_ver['pihole'], max_ver['pihole']])
+
+    if cur_ver['adminLTE'] != max_ver['adminLTE']:
+        admin_diff = True
+        diff_details.append(['AdminLTE', cur_ver['adminLTE'], max_ver['adminLTE']])
+
+    if cur_ver['FTL'] != max_ver['FTL']:
+        ftl_diff = True
+        diff_details.append(['FTL', cur_ver['FTL'], max_ver['FTL']])
+
+    version_diff = pihole_diff or admin_diff or ftl_diff
+
+
+    # if new version, check if the version_file exists
+    version_file_path = '/'.join(path, version_file)
+    version_file_exists = os.path.isfile(version_file_path)
+    if version_diff:
+        # if there is a new version, and we don't already have a version_file, 
+        # send a notification of a new version, and create the version_file
+        # if we have a version file already, do nothing
+        if not version_file_exists:
+            # TODO: send notification
+            headers = ['Component', 'Current Version', 'Latest Versioni']
+            update_details = tabulate(diff_details, headers, tablefmt='fancy_grid')
+            message = f"There is an update for Pi-Hole available:\n```{update_details}```"
+            content = {
+                "content":  message
+            }
+            requests.post(webhook, data=content)
+
+            # create version file
+            with open(version_file_path, 'w') as f:
+                f.write(max_ver)
+
+    # if there isn't a new version, but there is a version file, remove the version file
+    elif version_file_exists:
+        os.remove(version_file_path)
+
+
+
 # main logic:
 # if the directory is not mounted, error out and send notification
 # if the directory is mounted, but the backup file isn't present, 
@@ -85,7 +166,11 @@ if isDir:
     bkup_files = glob(search_path)
     if len(bkup_files) == 0:
         backup_not_found()
-    else:
+    # send inform message on sunday only
+    elif d.weekday() == 6:
         backup_found()
 else:
     nfs_not_mounted()
+
+# check for new version
+version_check()
